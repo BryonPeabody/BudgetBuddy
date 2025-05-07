4b1d5e8135776ee5b72061e096fa743c8e620c20
bryonpeabody <Bryon.Peabody@icloud.com>
2025-05-06 18:11:13 -0700

    Add login template, routing and redirects

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('expenses/', include('expenses.urls'))
+    path('expenses/', include('expenses.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
